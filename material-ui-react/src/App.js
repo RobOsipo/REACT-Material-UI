c42bd90ea5d9d5f0a33cbe83927d98e08f1a6e1e
Robert <osipovitchr@gmail.com>
2022-03-19 10:16:16 -0400

    ButtonGroups + Icons

--- material-ui-react/src/App.js
+++ material-ui-react/src/App.js
@@ -2,19 +2,35 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import SaveIcon from "@material-ui/icons/Save"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Button 
-      size='large'
-      onClick={() => console.log('hello')}
-      variant='contained' 
-      color="primary">
-        Hello world
-      </Button>
-
+    <ButtonGroup>
+        <Button 
+        size='large'
+        endIcon={<SaveIcon />}
+        
+        onClick={() => console.log('hello')}
+        variant='contained' 
+        color="primary">
+         Save
+        </Button>
+        <Button 
+        size='large'
+        endIcon={<SaveIcon />}
+        
+        onClick={() => console.log('hello')}
+        variant='contained' 
+        color="secondary">
+          Discard
+        </Button>
+        
+   </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
          
       </header>
